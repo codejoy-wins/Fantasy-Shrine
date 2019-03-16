2f25d6be8bf08220d0b76094d540da41045c9a46
Max <mjann@ucsc.edu>
2019-03-16 11:47:53 -0700

    seif baby

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import ben from './components/ben.jpg';
+import adam from './components/adam.jpg';
+
+import bigben from './components/bigben.jpg';
+import cmc from './components/cmc.jpg';
+import dhop from './components/dhop.jpg';
+import dj from './components/dj.jpg';
+import drake from './components/drake.jpg';
+import vance from './components/vance.jpeg';
+
+
+import Seif from './components/As';
 import './App.css';
 
 class App extends Component {
+  
   render() {
+    const mySty = {
+      "textDecoration": "none",
+      "color": 'goldenrod',
+    }
     return (
       <div className="App">
+        < Seif />
+        <img src={ben} className="App-logo" alt="logo" />
+        <h2>Big Ben</h2>
+        <img src={bigben} className="App-logow" alt="logo" />
+
+        <img src={adam} className="App-logo" alt="logo" />
+        <h2>Adam Thielen</h2>
+        <img src={adam} className="App-logow" alt="logo" />
+
+        <img src={drake} className="App-logo" alt="logo" />
+        <h2>Kenyan Drake</h2>
+        <img src={drake} className="App-logow" alt="logo" />
+
+        <img src={dj} className="App-logo" alt="logo" />
+        <h2>David Johnson</h2>
+        <img src={dj} className="App-logow" alt="logo" />
+
+        <img src={cmc} className="App-logo" alt="logo" />
+        <h2>Christian McCaffery</h2>
+        <img src={cmc} className="App-logow" alt="logo" />
+
+        <img src={dhop} className="App-logo" alt="logo" />
+        <h2>Deandre Hopkins</h2>
+        <img src={dhop} className="App-logow" alt="logo" />
+
+        <img src={vance} className="App-logo" alt="logo" />
+        <h2>Vance McDonald won the farm</h2>
+        <img src={vance} className="App-logow" alt="logo" />
+
+
+
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+             <a href="https://maxjann.com" style={mySty}>Jann Software</a>
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
---
+++ src/components/As.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+export class As extends Component {
+  render() {
+      const mySyt = {
+          "padding":"10px",
+          "backgroundColor": "blue",
+          "color": "white",
+
+          "margin-bottom": "4vw",
+      }
+    return (
+      <div style={mySyt}>
+        <h1> Elon's Blunt Musk </h1>
+        <h2> 2018 Westlake Fantasy Football Champion </h2>
+        <h3> Andrew Seif is batting 1000  </h3>
+
+
+      </div>
+    )
+  }
+}
+
+export default As
